e7ed7c0dfe5db14b082e4b82c0dda17375d403ff
MiyabiKakeru <masato.20110728@gmail.com>
2026-02-14 09:31:37 +0900

    Update h1 text based on device width

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const loading = document.querySelector("#loading")
 window.addEventListener("load", () => {
     loading.classList.add("loaded")
 })
-const pc = document.querySelector(".pc")
-if (window.matchMedia && window.matchMedia('(max-device-width: 640px)').matches) {
-    pc.remove()
+const h1 = document.querySelector("h1")
+if (window.matchMedia && window.matchMedia('(min-device-width: 800px)').matches) {
+    h1.textContent = "Miyabi Kakeru"
 }
