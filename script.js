00eb6fb62227c085641d37c8b0a31f97134d0655
MiyabiKakeru <masato.20110728@gmail.com>
2026-02-22 20:18:32 +0900

    Change h1 selector to use #title instead of h1

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const loading = document.querySelector("#loading")
 window.addEventListener("load", () => {
     loading.classList.add("loaded")
 })
-const h1 = document.querySelector("h1")
+const h1 = document.querySelector("#title")
 if (window.matchMedia && window.matchMedia('(min-device-width: 800px)').matches) {
     h1.textContent = "Miyabi Kakeru"
 }
